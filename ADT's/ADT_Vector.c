3179857fdc6ad328778f465d719f18fa8d275009
Marinosantellan <48847515+Marinosantellan@users.noreply.github.com>
2020-08-11 17:16:03 -0300

    Update ADT_Vector.c

--- ADT's/ADT_Vector.c
+++ ADT's/ADT_Vector.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define INIT_SIZE 100
+typedef enum 
+{
+	ERROR_NULL_POINTER,
+	ERROR_NO_MEMORY,
+	OK
+
+}status_t;
+
+#define INIT_SIZE 50
 
 typedef struct
 {
 
-	void **elements;
-	size_t len;	/*Tamaño efectivo del arreglo*/
-	size_t alloc_size; /*tamaño real del arreglo*/ /*dejar el alloc_size  ...*/
+	void **heap;
+	size_t len;	/*Tamaño efectivo del vector*/
+	size_t alloc_size; /*tamaño real del vector*/ 
 
 	destructor_t destructor;
 	comparator_t comparator;
@@ -17,10 +25,9 @@ typedef struct
 } ADT_Vector_t;
 
 /*Función que crea un ADT_Vector_t y se lo asigna al puntero recibido
-  el vector devuelto ya tiene memoria pedida e inicializada por default*/
-status_t ADT_Vector_create( ADT_Vector_t **vector)/*En realidad devuelve un ADT_Error_t*/
+  el vector devuelto ya tiene memoria pedida e inicializada con espacio para INIT_SIZE elementos*/
+status_t ADT_Vector_create( ADT_Vector_t **vector)
 {
-	size_t aux_size;
 
 	if( vector == NULL)
 	{
@@ -32,17 +39,16 @@ status_t ADT_Vector_create( ADT_Vector_t **vector)/*En realidad devuelve un ADT_
 		return ERROR_NO_MEMORY;
 	}
 
-	aux_size = INIT_SIZE;
-
-	if(((*vector)->elements = (void **) calloc( aux_size , sizeof(void *))) == NULL)
+	if(((*vector)->heap = (void **) calloc( INIT_SIZE , sizeof(void *))) == NULL)
 	{
-		ADT_Vector_destroy(vector);
+		free(*vector);
+		*vector = NULL;
 		return ERROR_NO_MEMORY;
 	}
 
 
 	(*vector)->len = 0;
-	(*vector)->alloc_size = aux_size;
+	(*vector)->alloc_size = INIT_SIZE;
 
 	(*vector)->destructor = NULL;
 	(*vector)->comparator = NULL;
@@ -51,7 +57,7 @@ status_t ADT_Vector_create( ADT_Vector_t **vector)/*En realidad devuelve un ADT_
 	return OK;
 }
 
-status_t ADT_Vector_destroy(ADT_Vector_t **vector)
+status_t ADT_Vector_destroy(ADT_Vector_t **vector) 
 {
 	size_t i;
 
@@ -62,14 +68,56 @@ status_t ADT_Vector_destroy(ADT_Vector_t **vector)
 
 	for( i = 0 ; i < (*vector)->len ; i++)
 	{
-		destructor(((*vector)->elements)[i]);
+		destructor(((*vector)->heap)[i]);
 	}
 
-	free(elements);
+	free(heap);
 
-	free(*vector);
-
+	free(*vector); 
 	*vector = NULL;
 
 	return OK;
+}
+
+/*Función que inserta un elemento al final del vector. En caso de que no haya mas espacio en este, se pedirá mas*/
+/*Si ocurre cualquier error el vector se destruye pero su respectivo puntero NO se inializa a NULL*/
+status_t ADT_Vector_append(ADT_Vector_t *vector, const void *ptr_obj)
+{
+	void *aux_ptr; 
+
+	if (vector == NULL || ptr_obj == NULL)
+	{
+		return ERROR_NULL_POINTER;
+	}
+
+	if(vector->len == vector->alloc_size)
+	{
+		if( ((aux_ptr = (void *) realloc( vector->heap , 2*(vector->alloc_size)) ) == NULL))
+		{
+			ADT_Vector_destroy(&vector);
+			return ERROR_NO_MEMORY;
+		}
+
+		vector->heap = aux_ptr;
+
+		(vector->alloc_size)*=2;
+	}
+
+	(vector->heap)[vector->len] = (void *) ptr_obj;
+
+	vector->len++;
+
+	return OK;
+}
+
+status_t 
+
+size_t ADT_Vector_get_true_size( const ADT_Vector_t *vector)
+{
+	return vector->alloc_size;
+}
+
+size_t ADT_Vector_get_used_size(const ADT_Vector_t *vector)
+{
+	return vector->len;
 }
